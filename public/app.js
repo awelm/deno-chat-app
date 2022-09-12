a55b135cec4a7098322b075257a7d0abb9868a6f
awelm <awelm@openai.com>
2022-09-11 17:21:31 -0700

    works properly

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-function askForName(){
+function askForName() {
   const person = prompt("Please enter your name");
   return person || "Anonymous";
 }
@@ -7,51 +7,70 @@ function escaped(s) {
   return $('<div></div>').html(s).html();
 }
 
-var name = askForName();
+const myUsername = askForName();
+let usernames = {};
 
-$('#data').attr('placeholder', 'send message as ' + name);
+$('#data').attr('placeholder', 'send message as ' + myUsername);
 
-var socket = io.connect('/');
+const socket = new WebSocket("ws://localhost:8080/ws_endpoint");
+
+function addMessage(username, message, boldPrefix = false) {
+  let prefix = escaped(username)
+  if (boldPrefix) {
+    prefix = '<b>' + prefix + '</b>'
+  }
+  $('#conversation').append( `${prefix}: ${escaped(message)} <br/>`);
+}
 
 // on connection to server, ask for user's name with an anonymous callback
-socket.on('connect', function () {
+socket.onopen = () => {
   // call the server-side function 'adduser' and send one parameter (value of prompt)
-  socket.emit('adduser', name);
-});
+  socket.send(JSON.stringify({
+    'event': 'add-user',
+    'username': myUsername,
+  }));
+};
 
-// listener, whenever the server emits 'updatechat', this updates the chat body
-socket.on('updatechat', function (username, data) {
-  $('#conversation').append('<b>' + escaped(username) + ':</b> ' + escaped(data) + '<br/>');
-});
+socket.onmessage = (m) => {
+  const data = JSON.parse(m.data);
 
-// listener, whenever the server emits 'updateusers', this updates the username list
-socket.on('updateusers', function (data) {
-  $('#users').empty();
-  $.each(data, function (key, value) {
-    $('#users').append('<div><a href="' + searchUrlFor(key) + '" target="_blank">' + key + '</div>');
-  });
-});
+  switch(data.event) {
+    case 'update-users':
+      newUsernames = data.usernames;
+      $('#users').empty();
+      $.each(newUsernames, function (username, _) {
+        if(username == myUsername) {
+          $('#users').append('<div><b>' + username + '</b></div>');
+        } else {
+          $('#users').append('<div>' + username + '</div>');
+        }
+      });
+      // update chat
+      for (const [username, _] of Object.entries(newUsernames)) {
+        if (username in usernames == false) {
+          addMessage('connected', username);
+        }
+      }
+      usernames = newUsernames;
+      break;
 
-socket.on('servernotification', function (data) {
-  var searchUrl = searchUrlFor(data.username);
-
-  if (data.connected) {
-    if (data.toSelf) data.username = 'you';
-    $('#conversation').append('connected: <a href="' + searchUrl + '" target="_blank">' + escaped(data.username) + '</a><br/>');
-  } else {
-    $('#conversation').append('disconnected: <a href="' + searchUrl + '" target="_blank">' + escaped(data.username) + '</a><br/>');
+    case 'update-chat':
+      addMessage(data.username, data.message, boldPrefix=true);
   }
-});
+};
 
 // on load of page
 $(function () {
-    // when the client hits ENTER on their keyboard
-    $('#data').keypress(function (e) {
-      if (e.which == 13) {
-        var message = $('#data').val();
-        $('#data').val('');
-        // tell server to execute 'sendchat' and send along one parameter
-        socket.emit('sendchat', message);
-      }
-    });
+  // when the client hits ENTER on their keyboard
+  $('#data').keypress(function (e) {
+    if (e.which == 13) {
+      var message = $('#data').val();
+      $('#data').val('');
+      // tell server to execute 'sendchat' and send along one parameter
+      socket.send(JSON.stringify({
+        'event': 'send-chat',
+        'message': message,
+      }))
+    }
+  });
 });
--- server.js
+++ server.js
@@ -1,13 +1,68 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { renderFileToString } from "https://deno.land/x/dejs/mod.ts";
+
+const usernames = {};
+const connectedClients = new Map();
+let currClientId = 0;
 
 const app = new Application();
 const port = 8080;
 
 const router = new Router();
 
-router.get("/", async (ctx) => {
-    ctx.response.body = await renderFileToString("./index.ejs"); // TODO: remove unnecessary ejs
+function broadcast(message) {
+    for (const client of connectedClients.values()) {
+        client.send(message);
+    }
+}
+
+function broadcast_usernames() {
+    console.log("Sending updated username list to all clients: " + JSON.stringify(usernames));
+    broadcast(JSON.stringify({
+        'event': 'update-users',
+        'usernames': usernames,
+    }));
+}
+
+router.get('/ws_endpoint', async ctx => {
+    const socket = await ctx.upgrade();
+
+    socket.onopen = () => {
+        currClientId++;
+        socket.clientId = currClientId;
+        connectedClients.set(currClientId, socket);
+        console.log(`New client connected: ${socket.clientId}`);
+    };
+
+    socket.onclose = () => {
+        console.log(`Client ID ${socket.clientId} disconnected (username: ${socket.username})`);
+        connectedClients.delete(socket.clientId);
+        delete usernames[socket.username];
+        broadcast_usernames();
+    };
+
+    socket.onerror = (e) => {
+        console.log(e instanceof ErrorEvent ? e.message : e.type);
+    };
+
+    socket.onmessage = (m) => {
+        const data = JSON.parse(m.data);
+
+        switch(data.event) {
+            case 'add-user':
+                const username = data.username
+                socket.username = username; 
+                usernames[username] = true;
+                broadcast_usernames();
+                break;
+            
+            case 'send-chat': 
+                broadcast(JSON.stringify({
+                    'event': 'update-chat',
+                    'username': socket.username,
+                    'message': data.message,
+                })); 
+        }
+    };
 });
 
 app.use(router.routes());
@@ -16,37 +71,7 @@ app.use(router.allowedMethods());
 app.use(async (context) => {
     await context.send({
         root: `${Deno.cwd()}/`,
-    });
-});
-
-io.sockets.on('connection', (socket) => {
-    socket.on('sendchat', (data) => {
-        io.sockets.emit('updatechat', socket.username, data);
-    });
-
-    socket.on('adduser', (username) => {
-        socket.username = username;
-
-        usernames[username] = username;
-
-        socket.emit(
-            'servernotification', {
-            connected: true,
-            toSelf: true,
-            username: username
-        });
-
-        socket.broadcast.emit('servernotification', { connected: true, username: username });
-
-        io.sockets.emit('updateusers', usernames);
-    });
-
-    socket.on('disconnect', () => {
-        delete usernames[socket.username];
-
-        io.sockets.emit('updateusers', usernames);
-
-        socket.broadcast.emit('servernotification', { username: socket.username });
+        index: "public/index.html",
     });
 });
 
