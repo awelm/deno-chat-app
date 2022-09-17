d2c8178e45fc47f09497de71976fbdccb650267e
awelm <awelm@openai.com>
2022-09-17 11:08:43 -0700

    format

--- public/app.js
+++ public/app.js
@@ -1,50 +1,53 @@
 const myUsername = prompt("Please enter your name") || "Anonymous";
-const socket = new WebSocket(`ws://localhost:8080/start_web_socket?username=${myUsername}`);
-
-function addMessage(username, message) {
-  $('#conversation').append( `<b> ${username} </b>: ${message} <br/>`);
-}
-
-function socketSend(obj) {
-  socket.send(JSON.stringify(obj));
-}
+const socket = new WebSocket(
+  `ws://localhost:8080/start_web_socket?username=${myUsername}`
+);
 
 socket.onopen = () => {
   socketSend({
-    'event': 'login'
-  })
+    event: "login",
+  });
 };
 
 socket.onmessage = (m) => {
   const data = JSON.parse(m.data);
 
-  switch(data.event) {
-    case 'update-users':
+  switch (data.event) {
+    case "update-users":
       // refresh displayed user list
-      $('#users').empty();
+      $("#users").empty();
       for (const username of data.usernames) {
-        $('#users').append('<div>' + username + '</div>');
+        $("#users").append("<div>" + username + "</div>");
       }
       break;
 
-    case 'send-message':
+    case "send-message":
       // display new chat message
       addMessage(data.username, data.message);
       break;
   }
 };
 
+function addMessage(username, message) {
+  // displays new message
+  $("#conversation").append(`<b> ${username} </b>: ${message} <br/>`);
+}
+
+function socketSend(obj) {
+  socket.send(JSON.stringify(obj));
+}
+
 // on page load
 $(function () {
   // when the client hits the ENTER key
-  $('#data').keypress(function (e) {
+  $("#data").keypress(function (e) {
     if (e.which == 13) {
-      var message = $('#data').val();
-      $('#data').val('');
+      var message = $("#data").val();
+      $("#data").val("");
       socketSend({
-        'event': 'send-message',
-        'message': message,
-      })
+        event: "send-message",
+        message: message,
+      });
     }
   });
 });
--- server.js
+++ server.js
@@ -6,68 +6,76 @@ const app = new Application();
 const port = 8080;
 const router = new Router();
 
+// send a message to all connected clients
 function broadcast(message) {
-    for (const client of connectedClients.values()) {
-        client.send(message);
-    }
+  for (const client of connectedClients.values()) {
+    client.send(message);
+  }
 }
 
+// send updated users list to all connected clients
 function broadcast_usernames() {
-    const usernames = [ ...connectedClients.keys()];
-    console.log("Sending updated username list to all clients: " + JSON.stringify(usernames));
-    broadcast(JSON.stringify({
-        'event': 'update-users',
-        'usernames': usernames,
-    }));
+  const usernames = [...connectedClients.keys()];
+  console.log(
+    "Sending updated username list to all clients: " + JSON.stringify(usernames)
+  );
+  broadcast(
+    JSON.stringify({
+      event: "update-users",
+      usernames: usernames,
+    })
+  );
 }
 
-router.get('/start_web_socket', async ctx => {
-    const socket = await ctx.upgrade();
-    const username = ctx.request.url.searchParams.get('username')
-    if (connectedClients.has(username)) {
-        socket.close(1008, `Username ${username} is already taken`);
-        return;
-    }
-    socket.username = username;
-    connectedClients.set(username, socket);
-    console.log(`New client connected: ${username}`);
-   
-    socket.onclose = () => {
-        console.log(`Client ${socket.username} disconnected`);
-        connectedClients.delete(socket.username);
+router.get("/start_web_socket", async (ctx) => {
+  const socket = await ctx.upgrade();
+  const username = ctx.request.url.searchParams.get("username");
+  if (connectedClients.has(username)) {
+    socket.close(1008, `Username ${username} is already taken`);
+    return;
+  }
+  socket.username = username;
+  connectedClients.set(username, socket);
+  console.log(`New client connected: ${username}`);
+
+  // when a client disconnects, remove them from the connected clients list
+  // and broadcast the active users list
+  socket.onclose = () => {
+    console.log(`Client ${socket.username} disconnected`);
+    connectedClients.delete(socket.username);
+    broadcast_usernames();
+  };
+
+  socket.onmessage = (m) => {
+    const data = JSON.parse(m.data);
+    switch (data.event) {
+      case "login":
+        // broadcast the active users list when a new user logs in
         broadcast_usernames();
-    };
+        break;
 
-    socket.onerror = (e) => {
-        console.log(e instanceof ErrorEvent ? e.message : e.type);
-    };
-
-    socket.onmessage = (m) => {
-        const data = JSON.parse(m.data);
-        switch(data.event) {
-            case 'login': 
-                broadcast_usernames();
-                break;
-
-            case 'send-message': 
-                broadcast(JSON.stringify({
-                    'event': 'send-message',
-                    'username': socket.username,
-                    'message': data.message,
-                })); 
-                break;
-        }
-    };
+      case "send-message":
+        // broadcast new message if someone sent one
+        broadcast(
+          JSON.stringify({
+            event: "send-message",
+            username: socket.username,
+            message: data.message,
+          })
+        );
+        break;
+    }
+  };
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(async (context) => {
-    await context.send({
-        root: `${Deno.cwd()}/`,
-        index: "public/index.html",
-    });
+  await context.send({
+    root: `${Deno.cwd()}/`,
+    index: "public/index.html",
+  });
 });
 
-console.log('Listening at http://localhost:' + port);
+console.log("Listening at http://localhost:" + port);
 await app.listen({ port });
